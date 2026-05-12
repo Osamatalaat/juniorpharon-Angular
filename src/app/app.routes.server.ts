5ead881c81710299a2a5fcdd594eb97db61c970e
Osamatalaat <osamatalaat707@gmail.com>
2026-05-13 01:33:59 +0300

    ssr removed

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,38 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // 🟢 static pages
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+ 
+  {
+    path: 'trips',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'packages',
+    renderMode: RenderMode.Prerender
+  },
+
+  // 🔵 dynamic SSR pages
+  {
+    path: 'trips/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'packages/:id',
+    renderMode: RenderMode.Server
+  },
+
+  // 🟡 runtime pages (auth + booking)
+  {
+    path: 'booking',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'auth',
+    renderMode: RenderMode.Server
   }
 ];
